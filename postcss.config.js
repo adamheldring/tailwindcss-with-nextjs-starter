2a4bf90749db709abd0f01e2818289d14fa2e8e4
A-Game <javascript@adamheldring.com>
2020-03-29 11:01:22 +0200

    optiomize production builds by automatically removing unused classes with purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,28 @@
 module.exports = {
-  plugins: ["tailwindcss", "postcss-preset-env"]
+  plugins: [
+    "tailwindcss",
+    "postcss-preset-env",
+    [
+      "@fullhuman/postcss-purgecss", 
+      process.env.NODE_ENV === "production" 
+        ? {
+          content: [
+            './src/**/*.html',
+            './src/**/*.htm',
+            './src/**/*.js',
+            './src/**/*.jsx',
+            './src/**/*.ts',
+            './src/**/*.tsx',
+          ],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        } : false, 
+    ]
+  ]
 }
+
+// module.exports = {
+//   plugins: [
+//     "tailwindcss",
+//     "postcss-preset-env",
+//   ]
+// }
